b57f9b5d5aa3bc37e245158ab94ce7dba49d42e1
to be removed. see /~jhthorsen <pm@flodhest.net>
2009-03-11 19:44:43 +0000

    Import of EIDOLON/YAML-Object-0.04 from CPAN.
    gitpan-cpan-distribution: YAML-Object
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         EIDOLON/YAML-Object-0.04.tar.gz
    gitpan-cpan-author:       EIDOLON
    gitpan-cpan-maturity:     released

--- lib/YAML/Object.pm
+++ lib/YAML/Object.pm
@@ -1,120 +1,4 @@
-
-#====================
 package YAML::Object;
-#====================
-
-use warnings;
-use strict;
-use YAML;
-use overload (
-    q(%{})   => sub {
-        my $data = &{ $_[0] };
-        return $data->[0] if(UNIVERSAL::isa($data->[0], "HASH"));
-        return {};
-    },
-    q(@{})   => sub {
-        my $data = &{ $_[0] };
-        return $data->[0] if(UNIVERSAL::isa($data->[0], "ARRAY"));
-        return [];
-    }, 
-    q(${})   => sub { return $_[0]->()->[0] },
-    q("")    => sub { return $_[0]->()->[0] },
-    fallback => 1,
-);
-
-our $VERSION = '0.03';
-our($AUTOLOAD, $yaml_object, $yaml_ok, $get_key);
-
-$yaml_ok = sub { #============================================================
-    return 0 if(/^\s*\-{3}/);
-    return 0 if(/^\s*#/);
-    return 0 if(/^\s*$/);
-    return 1;
-};
-
-$yaml_object = sub { #========================================================
-    my $in   = shift;
-    my $data = [undef, undef, q()];
-    my $yaml = q();
-    my $fh;
-
-    unless(defined $in) {
-        return sub { $data };
-    }
-    elsif(ref $in eq 'GLOB') {
-        $fh = $in;
-    }
-    elsif(ref $in eq 'HASH') {
-        $data->[0] = $in;
-    }
-    elsif($in !~ /\n/ and -r $in) {
-        open($fh, "<", $in) or die "Could not read yaml-file ($in): $!\n";
-    }
-    else {
-        $yaml = $in;
-    }
-
-    if($fh) {
-        local $_;
-        while($_ = readline $fh) {
-            $yaml_ok->() and $yaml .= $_;
-        }
-    }
-
-    if($yaml) {
-        $data->[0] = Load($yaml);
-    }
-
-    return bless sub { $data };
-};
-
-$get_key = sub { #============================================================
-    my $self = shift;
-    my $data = $self->();
-
-    if($data->[1]) {
-        return join q(->), $get_key->($data->[1]), $data->[2];
-    }
-    else {
-        return q();
-    }
-};
-
-sub AUTOLOAD { #==============================================================
-    my $self = shift;
-    my $data = &$self->[0];
-    my($key) = $AUTOLOAD =~ /::(\w+)$/mx;
-    my $next = [undef, $self, $key];
-
-    unless(defined $data) {
-        die(sprintf "YAML::Object: %s->%s does not exist\n",
-            $get_key->($self), $key,
-        );
-    }
-
-    if(ref $data eq 'HASH') {
-        $next->[0] = $data->{$key};
-    }
-    elsif(ref $data eq 'ARRAY') {
-        $key       =~ s/\D//g;
-        $next->[0] =  $data->[$key];
-    }
-
-    return bless sub { $next };
-}
-
-sub import { #================================================================
-    my $parent = caller(0);
-    no strict 'refs';
-    *{"${parent}::yaml_object"} = $yaml_object;
-}
-
-DESTROY { #===================================================================
-}
-
-#=============================================================================
-1;
-__END__
 
 =head1 NAME
 
@@ -122,7 +6,7 @@ YAML::Object - Use OO to point to a yaml-node
 
 =head1 VERSION
 
-0.03
+0.04
 
 =head1 SYNOPSIS
 
@@ -154,7 +38,40 @@ Both should behave the same way in your code.
 
 The above should behave the same way in your code.
 
-=head1 FUNCTIONS
+=cut
+
+use warnings;
+use strict;
+use Symbol;
+use YAML;
+use overload (
+    q(%{}) => sub {
+        my $data = $_[0]->()->[0];
+        return $data if(UNIVERSAL::isa($data, "HASH"));
+        return {};
+    },
+    q(@{}) => sub {
+        my $data = $_[0]->()->[0];
+        return $data if(UNIVERSAL::isa($data, "ARRAY"));
+        return [];
+    }, 
+    q(*{}) => sub {
+        my $s = gensym();
+        my $d = $_[0]->()->[0];
+
+        *$s->{ref($d) || 'SCALAR'} = $d;
+
+        return $s;
+    },
+    q(${}) => sub { $_[0]->()->[0] },
+    q("") => sub { $_[0]->()->[0] },
+    fallback => 1,
+);
+
+our $VERSION = '0.04';
+our($AUTOLOAD, $yaml_object, $get_key);
+
+=head1 EXPORTED FUNCTIONS
 
 =head2 yaml_object(string)
 
@@ -167,10 +84,97 @@ Object constructor. Takes one argument, which can be either:
 
 Returns a C<YAML::Object> object.
 
+=cut
+
+$yaml_object = sub {
+    my $in   = shift;
+    my $data = [undef, undef, q()];
+    my $yaml = q();
+    my $fh;
+
+    unless(defined $in) {
+        return sub { $data };
+    }
+    elsif(ref $in eq 'GLOB') {
+        $fh = $in;
+    }
+    elsif(ref $in eq 'HASH') {
+        $data->[0] = $in;
+    }
+    elsif($in !~ /\n/ and -r $in) {
+        open($fh, "<", $in)
+            or die "YAML::Object: Could not read yaml-file ($in): $!\n";
+    }
+    else {
+        $yaml = $in;
+    }
+
+    if($fh) {
+        local $_;
+        $yaml .= $_ while(<$fh>);
+    }
+
+    if($yaml) {
+        $data->[0] = Load($yaml);
+    }
+
+    return bless sub { $data };
+};
+
+sub AUTOLOAD {
+    my $self = shift;
+    my($key) = $AUTOLOAD =~ /::(\w+)$/;
+
+    return if($key eq 'DESTROY');
+
+    my $data = $self->()->[0];
+    my $next = [undef, $self, $key];
+
+    unless(defined $data) {
+        die(sprintf "YAML::Object: %s->%s does not exist\n",
+            $get_key->($self), $key,
+        );
+    }
+
+    if(ref $data eq 'HASH') {
+        $next->[0] = $data->{$key};
+    }
+    elsif(ref $data eq 'ARRAY') {
+        $key =~ s/\D//g;
+
+        unless(length $key) {
+            $key = $AUTOLOAD =~ /::(\w+)$/;
+            die "YAML::Object: '$key' is not numeric!" 
+        }
+
+        $next->[0] = $data->[$key];
+    }
+
+    return bless sub { $next }, ref $self;
+}
+
+sub import {
+    no strict 'refs';
+    *{caller(0) ."::yaml_object"} = $yaml_object;
+}
+
+$get_key = sub {
+    my $self = shift;
+    my $data = $self->();
+
+    if($data->[1]) {
+        return join q(->), $get_key->($data->[1]), $data->[2];
+    }
+    else {
+        return q();
+    }
+};
+
 =head1 METHODS
 
 All methods called, will map to an element in the YAML-tree or undef / throw
-error if not.
+error if not. With two exceptions: AUTOLOAD and import are in use in this
+package.
 
 =head1 OVERLOADING
 
@@ -203,3 +207,5 @@ This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
+
+1;
